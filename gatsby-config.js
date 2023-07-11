97146bdcf527751063e35495fef52f0bcc5f9b36
Ryan Doherty <ryan@epicsea.com>
2023-07-11 12:32:39 -0400

    GA plugin
    - adding GA plugin to config file and dev dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
     title: `J. Ryan Doherty Design`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `Web design and developent for artists, small businesses, contractors and non-profits.`,
     author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    siteUrl: `https://jryandoherty.com/`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -15,6 +15,39 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+
+    {
+          resolve: `gatsby-plugin-google-gtag`,
+          options: {
+            // You can add multiple tracking ids and a pageview event will be fired for all of them.
+            trackingIds: [
+              "G-HJTVZF928W", // Google Analytics / GA
+
+            ],
+            // This object gets passed directly to the gtag config command
+            // This config will be shared across all trackingIds
+            gtagConfig: {
+              optimize_id: "OPT_CONTAINER_ID",
+              anonymize_ip: true,
+              cookie_expires: 0,
+            },
+            // This object is used for configuration specific to this plugin
+            pluginConfig: {
+              // Puts tracking script in the head instead of the body
+              head: false,
+              // Setting this parameter is also optional
+              respectDNT: true,
+              // Avoids sending pageview hits from custom paths
+              exclude: ["/preview/**", "/do-not-track/me/too/"],
+              // Defaults to https://www.googletagmanager.com
+              origin: "https://www.googletagmanager.com/gtag/js?id=G-HJTVZF928W",
+              // Delays processing pageview events on route update (in milliseconds)
+              delayOnRouteUpdate: 0,
+            },
+          },
+        },
+
+
     `gatsby-plugin-sass`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
