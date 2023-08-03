ee064fa20987e24699b7eed449702c6bbd88f2a5
Ryan Doherty <ryan@epicsea.com>
2023-08-02 21:13:55 -0400

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -16,7 +16,95 @@ exports.createPages = async ({ graphql, actions }) => {
 	createRedirect({
     fromPath: `/portfolio/emails/office_cubicles.php`,
     toPath: `/portfolio/`,
+  })
+
+  createRedirect({
+
+    fromPath: `/portfolio/emails/cloud.php`,
+    toPath: `/portfolio/`,
+  })
+
+createRedirect({
+    fromPath: `/portfolio/emails/bucket.php`,
+    toPath: `/portfolio/`,
+  })
+createRedirect({
+    fromPath: `/portfolio/emails/trucks.php`,
+    toPath: `/portfolio/`,
+  })
+createRedirect({
+
+    fromPath: `/portfolio/emails/bdc.php`,
+    toPath: `/emails/bdc/`,
+  })
+
+createRedirect({
+    fromPath: `/portfolio/conversion/hornet.php`,
+    toPath: `/portfolio/`,
+  })
+
+  createRedirect({
+    fromPath: `/portfolio/conversion/copernicus.php`,
+    toPath: `/portfolio/`,
+  })
+
+createRedirect({
+    fromPath: `/portfolio/conversion/construction-portal.php`,
+    toPath: `/portfolio/`,
+  })
+
+createRedirect({
+    fromPath: `/portfolio/conversion/truck_portal.php`,
+    toPath: `/portfolio/`,
+  })
+
+createRedirect({
+    fromPath: `/portfolio/conversion/bdc_price_quotes.php`,
+    toPath: `/conversion/bdc-price-quotes/`,
+  })
+
+createRedirect({
+    fromPath: `/portfolio/sites/avian_records.php`,
+    toPath: `/sites/avian-records/`,
+  })
+
+createRedirect({
+    fromPath: `/portfolio/sites/bdc_community.php`,
+    toPath: `/sites/bdc-community/`,
+  })
+
+createRedirect({
+    fromPath: `/portfolio/sites/bdc_partner.php`,
+    toPath: `/portfolio/`,
+  })
+
+createRedirect({
+    fromPath: `/portfolio/sites/buyerzone_home_2015.php`,
+    toPath: `/sites/buyerzone-2015/`,
+  })
+
+createRedirect({
+    fromPath: `/portfolio/sites/grab_cad_blog.php`,
+    toPath: `/sites/grabcad/`,
+  })
+
+createRedirect({
+    fromPath: `/portfolio/sites/buyerzone_leads_2016.php`,
+    toPath: `/portfolio/`,
+  })
+
+  createRedirect({
+    fromPath: `/portfolio/sites/louis_mian.php`,
+    toPath: `/sites/louis-mian/`,
+  })
+createRedirect({
+    fromPath: `/portfolio/sites/roger_buttles.php`,
+    toPath: `/sites/roger-buttles/`,
+    })
+createRedirect({
+    fromPath: `/portfolio/sites/dzone.php`,
+    toPath: `/portfolio/`,
+    })
 
 
-  });
 }
